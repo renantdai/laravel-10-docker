0df089b25e38276ccd45cb6f2a68d57449b3b612
renantdai <renankh@hotmail.com>
2023-11-07 06:48:13 -0300

    fix: corrige rota

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
 Route::put('/supports/{id}', [ SupportController::class, 'update'])->name('supports.update');
 Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
-Route::get('/suupports/create', [SupportController::class, 'create'])->name('supports.create');
+Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
 Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
 Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');;
 
